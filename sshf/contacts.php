1d784725c20320f403c21f14bf8cb30259243599
Tor Arne <torabang@gmail.com>
2016-02-11 10:01:52 +0100

    fix

---
+++ sshf/contacts.php
@@ -0,0 +1,184 @@
+<?php
+include_once 'includes/db_connect.php';
+include_once 'includes/functions.php';
+ 
+sec_session_start();
+ 
+if (login_check($mysqli) == true) {
+    $logged = 'in';
+} else {
+    $logged = 'out';
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Contacts</title>
+<meta charset="utf-8">
+<meta name="format-detection" content="telephone=no" />
+<link rel="stylesheet" href="css/contact-form.css">
+<link rel="stylesheet" href="css/style.css">
+<script src="js/script.js"></script>
+<script>
+ $(window).load(function(){
+  $().UItoTop({ easingType: 'easeOutQuart' });
+ });
+</script>
+</head>
+<body class="page1" id="top">
+<!--==============================
+              header
+=================================-->
+<div class="main">
+<header>
+<div class="topplinje"><a class="button" href="login.php"><?php 
+    if(login_check($mysqli) == true){ 
+        echo $_SESSION['username'];
+        echo '<a href="logout.php"><span>&nbsp;Logout</span></a></li>';
+        }
+    elseif(login_check($mysqli) == false) 
+        echo '<a href="register.php"><span>Login/Register</span></a></li>';
+    ?></a></div>
+  <div class="container_12">
+		<img src="images/logo.png"  />
+  <section id="stuck_container">
+  <!--==============================
+              Stuck menu
+  =================================-->
+    <div class="container_12">
+        <div class="grid_12">
+          <div class="navigation ">
+            <nav>
+              <ul class="sf-menu">
+               <li><a href="index.php">Hjem</a></li>
+               <li><a href="documents.php">Dokumenter</a></li>
+               <li><a href="about.php">Om oss</a></li>
+               <li><a href="environment.php">Miljø</a></li>
+               <li class="current"><a href="contacts.php">Kontakt</a></li>
+             </ul>
+            </nav>
+            <br><br>
+		  <hr class="skille">
+         <div class="clear"></div>
+     </div>
+     <div class="clear"></div>
+    </div>
+  </section>
+</header>
+<!--=====================
+          Content
+======================-->
+<section id="content">
+  <div class="container_12">
+    <div class="grid_12">
+      <h2 class="inset__1">Finn oss!</h2>
+      <div class="map">
+        <figure class="">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d67374.31251812285!2d7.966144266196378!3d58.149475125930685!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x463802fcf6a52cbf%3A0x654fb6cea880de8f!2sEgsveien+100%2C+4615+Kristiansand!5e0!3m2!1sno!2sno!4v1454326598978" style="border:0"></iframe>
+        </figure>
+      </div>
+    </div>
+    <div class="grid_5">
+      <h4 class="color1 inset__1">Kontakt Info</h4>
+      <p>Sørlandet sykehus<br>
+	Telefon sentralbord: 03738<br>
+	Bruk av nettsted<br>
+	Personvernpolicy<br>
+
+	Postadresse <br>
+	Pb. 416, 4604 Kristiansand<br>
+
+	Besøksadresser<br>
+
+	Organisasjonsnummer: 983 975 240<br>
+	Nettredaktør: Rune N. Jonassen<br>
+      E-mail: <a href="mailto:mail@demolink.org">postmottak@sshf.no</a></p>
+    </div>
+    <div class="grid_6 prefix_1">
+      <h4 class="color1 inset__1">Kontakt Form</h4>
+      <form id="contact-form">
+          <div class="contact-form-loader"></div>
+          <fieldset>
+            <label class="name">
+              <input type="text" name="name" placeholder="Navn:" value="" data-constraints="@Required @JustLetters"  />
+              <span class="empty-message">*This field is required.</span>
+              <span class="error-message">*This is not a valid name.</span>
+            </label>
+            <label class="email">
+              <input type="text" name="email" placeholder="E-mail:" value="" data-constraints="@Required @Email" />
+              <span class="empty-message">*This field is required.</span>
+              <span class="error-message">*This is not a valid email.</span>
+            </label>
+            <label class="phone">
+              <input type="text" name="phone" placeholder="Telefon:" value="" data-constraints="@Required @JustNumbers" />
+              <span class="empty-message">*This field is required.</span>
+              <span class="error-message">*This is not a valid phone.</span>
+            </label>
+            <label class="message">
+              <textarea name="message" placeholder="Melding:" data-constraints='@Required @Length(min=20,max=999999)'></textarea>
+              <span class="empty-message">*This field is required.</span>
+              <span class="error-message">*The message is too short.</span>
+            </label>
+            <div class="ta__right">
+              <a href="#" class="link-1" data-type="reset">Tøm</a>
+              <a href="#" class="link-1" data-type="submit">Send</a>
+            </div>
+          </fieldset>
+          <div class="modal fade response-message">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                  <h4 class="modal-title">Modal title</h4>
+                </div>
+                <div class="modal-body">
+                  You message has been sent! We will be in touch soon.
+                </div>
+              </div>
+            </div>
+          </div>
+        </form>
+    </div>
+    <div class="clear"></div>
+  </div>
+</section>
+<!--==============================
+              footer_top
+=================================-->
+<div class="footer-top">
+  <div class="container_12">
+    <div class="grid_12">
+      <div class="sep-1"></div>
+    </div>
+    <div class="grid_4">
+      <address class="address-1"> <div class="fa fa-home"></div>Egsveien 100, 4615 Kristiansand,  <br>
+Telefon: 03738</address>
+    </div>
+    <div class="grid_3">
+      <a href="#" class="mail-1"><span class="fa fa-envelope"></span>Knut-Kristian.Aas.Bjornstad@sshf.no</a>
+    </div>
+    <div class="grid_4 fright">
+      <div class="socials">
+        <a href="#">facebook</a>
+        <a href="#">twitter</a>
+        <a href="#">google+</a>
+      </div>
+    </div>
+    <div class="clear"></div>
+  </div>
+</div>
+<!--==============================
+              footer
+=================================-->
+</div>
+<footer id="footer">
+  <div class="container_12">
+    <div class="grid_12">
+      <div class="sub-copy">4400 &copy; <span id="copyright-year"></span> | <a href="#">Privacy Policy</a> <br> Nettsiden er laget som ett bachelor prosjekt av <a href="https://www.facebook.com/steffangraf" rel="nofollow">group 4400</a></div>
+    </div>
+    <div class="clear"></div>
+  </div>
+</footer>
+<a href="#" id="toTop" class="fa fa-angle-up"></a>
+</body>
+</html>
