280c129d476e17f57c8835c6486ce7c9fedc2c6b
pjfedog <julianwf@yahoo.no>
2016-03-17 12:40:30 +0100

    Fortsettelse på miljøkalkulator
    Nå kan du velge mellom variabler som skal kalkuleres
    Må gjøres:
    -Dynamiske variabler
    -Utseende

--- sshf/calculator.php
+++ sshf/calculator.php
@@ -12,9 +12,9 @@
 <form method='post' action='calculator.php'>
 <input type='text' name='value1'>
  
-<select name='value2'>
-<option>Engangsglass</option>
-<option>Kilometer kjørt (Bensinforbruk)</option>
+<select name='forbruk'>
+<option value2="Engangsglass">Engangsglass</option>
+<option value3="Bensinforbruk">Bensinforbruk</option>
 
 
 </select>
@@ -27,24 +27,46 @@
 //Input integer in value1, static value2, 
 if(isset($_POST['submit'])){
 $value1 = $_POST['value1'];
+$forbruk = $_POST['forbruk'];
 $value2 = "5";
+$value3 = "1.16";
 
 
 //Hvis ganger-tegnet er valgt, så ganger value1(dynamisk) med value2(statisk)
-{
+if($forbruk=="Engangsglass"){
 echo "<b>Du bruker engangsglass for verdi:</b><br>";
 echo $value1*$value2 . " kroner"; 
 }
 
-{
+if($forbruk=="Engangsglass"){
 echo "<br><b>I en arbeidsuke blir dette:</b><br>";
 echo $value1*$value2*5 . " kroner"; 
 }
 
-{
+if($forbruk=="Engangsglass"){
 echo "<br><b>I et arbeidsår (230 dager) blir dette:</b><br>";
 echo $value1*$value2*230 . " kroner"; 
 }
+
+
+if($forbruk=="Bensinforbruk"){
+echo "<b>Du bruker bensin for verdi:</b><br>";
+echo $value1*$value3 . " kroner"; 
+}
+
+if($forbruk=="Bensinforbruk"){
+echo "<br><b>I en arbeidsuke blir dette:</b><br>";
+echo $value1*$value3*5 . " kroner"; 
+}
+
+if($forbruk=="Bensinforbruk"){
+echo "<br><b>I et arbeidsår (230 dager) blir dette:</b><br>";
+echo $value1*$value3*230 . " kroner"; 
+}
+
+
+
+
 
 }
 
