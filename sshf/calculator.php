dbc77e4e40dc8f802ead93abb57b2bd9add7b58e
pjfedog <julianwf@yahoo.no>
2016-03-18 10:14:57 +0100

    Calculator fortsettelse
    Opprydding i kode.
    Mangler utseende og dynamiske funksjoner enda.

--- sshf/calculator.php
+++ sshf/calculator.php
@@ -1,14 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-
+<title>Miljøkalkulator</title>
 <meta charset="utf-8">
 <meta name="format-detection" content="telephone=no" />
-<title>Miljøkalkulatoren</title>
+
+
+
+
 
 </head>
 <body>
 <text>Hvor mange engangsglass bruker du per dag?</text>
+<text><br>Hvor mange kilometer kjører du per dag?</text>
+
 <form method='post' action='calculator.php'>
 <input type='text' name='value1'>
  
@@ -24,15 +29,15 @@
 <input type='submit' name='submit' value='Kalkuler'></form>
 
 <?php
-//Input integer in value1, static value2, 
+//Input integer/tall i value1, static value2,3 
 if(isset($_POST['submit'])){
 $value1 = $_POST['value1'];
 $forbruk = $_POST['forbruk'];
-$value2 = "5";
+$value2 = "1";
 $value3 = "1.16";
 
 
-//Hvis ganger-tegnet er valgt, så ganger value1(dynamisk) med value2(statisk)
+//Hvis engangsglass er valgt, gang value1(dynamisk) med value2(statisk)
 if($forbruk=="Engangsglass"){
 echo "<b>Du bruker engangsglass for verdi:</b><br>";
 echo $value1*$value2 . " kroner"; 
@@ -48,7 +53,7 @@ echo "<br><b>I et arbeidsår (230 dager) blir dette:</b><br>";
 echo $value1*$value2*230 . " kroner"; 
 }
 
-
+//Hvis bensinforbuk er valgt, gang value1(dynamisk) med value3(statisk)
 if($forbruk=="Bensinforbruk"){
 echo "<b>Du bruker bensin for verdi:</b><br>";
 echo $value1*$value3 . " kroner"; 
@@ -74,3 +79,4 @@ echo $value1*$value3*230 . " kroner";
 
 </body>
 </html>
+
