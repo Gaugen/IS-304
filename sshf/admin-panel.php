fe397bb7b1218d80d30b41db6370f7399a02ef0f
pjfedog <julianwf@yahoo.no>
2016-03-30 11:46:24 +0200

    Calculator admin
    La til calculatorfunksjon i admin-panel

--- sshf/admin-panel.php
+++ sshf/admin-panel.php
@@ -44,7 +44,111 @@ if (login_check($mysqli) == true) {
 
 	<section id="tab1">
 		<h2><a href="#tab1">Tab 1</a></h2>
-		asd
+		<text>Her kan du legge inn nye verdier</text> 
+<form method='post' name='engangsglass'> 
+<input type='int' name='value2'>
+<input type='submit' name='engangsglass' value='Legg til verdi'></form>
+<br></br>
+
+<text>Her kan du legge inn nye verdier</text><b></br>
+<form method='post' name='bensinforbruk'>
+<input type='int' name='value3'>
+<input type='submit' name='bensinforbruk' value='Legg til verdi'></form>
+<br></br>
+
+<?php 
+// Oppdater verdien i engangsglass i calculator-tabellen
+if(isset($_POST['engangsglass'])){
+	$q = "UPDATE calculator SET engangsglass='$_POST[value2]'";
+	mysqli_query($mysqli, $q);
+}
+
+	
+// Oppdater verdien i bensinforbruk i calculator-tabellen	
+if(isset($_POST['bensinforbruk'])){
+	$r = "UPDATE calculator SET bensinforbruk='$_POST[value3]'";
+	mysqli_query($mysqli, $r);
+}
+
+?>
+<!-->Bruker <-->
+<text>Hvor mange engangsglass bruker du per dag?</text>
+<text><br>Hvor mange kilometer kjører du per dag?</text>
+
+<form method='post'>
+<input type='text' name='value1'>
+ 
+<select name='forbruk'>
+<option value2="Engangsglass">Engangsglass</option>
+<option value3="Bensinforbruk">Bensinforbruk</option>
+
+
+</select>
+
+
+
+<input type='submit' name='submit' value='Kalkuler'></form>
+
+<?php
+
+
+
+//Input tall i value1, hent databaseverdier for value 2 og 3 
+if(isset($_POST['submit'])){
+
+$sql = "SELECT * FROM calculator";
+$result_set=mysqli_query($mysqli, $sql);
+$x=mysqli_fetch_array($result_set); 	
+	
+$value1 = $_POST['value1'];
+$forbruk = $_POST['forbruk'];
+$value2 = $x['engangsglass'];
+$value3 = $x['bensinforbruk'];
+
+
+//Hvis engangsglass er valgt, gang value1(dynamisk) med value2(fra database)
+if($forbruk=="Engangsglass"){
+echo "<b>Du bruker engangsglass for verdi:</b><br>";
+echo $value1*$value2 . " kroner"; 
+}
+
+
+if($forbruk=="Engangsglass"){
+echo "<br><b>I en arbeidsuke blir dette:</b><br>";
+echo $value1*$value2*5 . " kroner"; 
+}
+
+if($forbruk=="Engangsglass"){
+echo "<br><b>I et arbeidsår (230 dager) blir dette:</b><br>";
+echo $value1*$value2*230 . " kroner"; 
+}
+
+
+//Hvis bensinforbruk er valgt, gang value1(dynamisk) med value3(fra database)
+if($forbruk=="Bensinforbruk"){
+echo "<b>Du bruker bensin for verdi:</b><br>";
+echo $value1*$value3 . " kroner"; 
+}
+
+
+if($forbruk=="Bensinforbruk"){
+echo "<br><b>I en arbeidsuke blir dette:</b><br>";
+echo $value1*$value3*5 . " kroner"; 
+}
+
+if($forbruk=="Bensinforbruk"){
+echo "<br><b>I et arbeidsår (230 dager) blir dette:</b><br>";
+echo $value1*$value3*230 . " kroner"; 
+}
+
+
+
+
+
+}
+
+
+?>
 	</section>
 	
 	<section id="tab2">
