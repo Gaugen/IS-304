0c1bb5a0b7cf88e4e6c91eaf2adb69f0ae82f30c
pjfedog <julianwf@yahoo.no>
2016-05-18 11:15:12 +0200

    Default image
    La til funksjon som gir et default bilde når poster ikke har bilde
    vedlagt

--- sshf/image.php
+++ sshf/image.php
@@ -4,13 +4,19 @@ include_once 'includes/functions.php';
  
 
 
-$newsno = (isset($_GET['newsno']) ? $_GET['newsno'] : null);
+$newsno = isset($_GET['newsno']) ? $_GET['newsno'] : null;
 $q = "SELECT newsphoto,newsphototype FROM post where newsno='$newsno'";
 $r = mysqli_query($mysqli,"$q");
 if($r)
 {
-
 $row = mysqli_fetch_array($r);
+	//Hvis row er tom, kjør ny spørring
+if(empty($row['newsphoto'])) {
+	$q = "SELECT newsphoto,newsphototype FROM post where newsno='1' LIMIT 1";
+	$r = mysqli_query($mysqli,"$q");
+	// ID 1 should always exist.
+	$row = mysqli_fetch_array($r);
+}
 $type = "Content-type: ".$row['newsphototype'];
 header($type);
 echo $row['newsphoto'];
