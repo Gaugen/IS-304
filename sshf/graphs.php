9535e3b87c6d7fa955c12702e0605c02f2f6664f
Gaugen <torabang@gmail.com>
2016-05-18 10:40:15 +0200

    La til edit text på søylediagram siden

--- sshf/graphs.php
+++ sshf/graphs.php
@@ -30,10 +30,67 @@ sec_session_start();
 ======================-->
 <section id="content">
 <div class="container_12">
+    <?php
+	
+	if(isset($_POST['update'])){
+	$topic = $_POST['teksttopic'];
+	$info = $_POST['tekstinfo'];
+		$stmt = $mysqli->prepare("UPDATE tekstbokser SET teksttopic = ?, tekstinfo = ?
+											WHERE plassering = 'Graphs'
+											LIMIT 1");
+		$stmt->bind_param('ss', $topic, $info);  
+		$stmt->execute();    // Execute the prepared query.
+	}
+	if ($stmt =$mysqli->prepare("SELECT teksttopic, tekstinfo
+        FROM tekstbokser
+		WHERE plassering = 'Graphs'
+        LIMIT 1")) {
+        $stmt->execute();    // Execute the prepared query.
+        $stmt->store_result();
+ 
+        // get variables from result.
+        $stmt->bind_result($overskrift, $tekst);
+        $stmt->fetch();
+		}
+	else
+	{
+		echo " Virket ikke!";
+	}
+	?>
+
 <br>
-<br>
+
 <center>
-<h4>Ekko tekst</h3>
+<br>
+<?php if (login_check($mysqli) == true) {?>
+<br>
+<form action="graphs.php" method="POST">
+	  <h2 class="inset__1"><?php echo nl2br ($overskrift); ?></h2>
+	  <p><font size="4"><?php echo nl2br ($tekst); ?></p></font>
+<div class="dropdownIndex">
+<submit onclick="myIndexFunction()" class="dropbtnIndex"><font size="3" color="blue" style="text-decoration: underline">Rediger teksten.</font></submit>
+  <div id="myDropdownIndex" class="dropdown-contentIndex">
+  <table <div class=containerpost border=0 align=center bgcolor=#d0d7e9>
+  <tr>
+  <td>Overskrift</td><td><input type=text size="35%" name="teksttopic" value="<?php echo     $overskrift;?>"></td>
+  </tr>
+  <tr>
+  <td>Informasjon</td><td><textarea name="tekstinfo" rows="10" cols="60"><?php echo $tekst;?>     </textarea></td>
+  </tr>
+  <tr>
+  <td></td><td><input type="submit" name="update" value="Post"></td>
+  </tr>
+  </table>
+  </div>
+</div>
+</form>
+<?php }
+	  else{?>
+	  <br>
+	  <h2 class="inset__1"><?php echo nl2br ($overskrift); ?></h2>
+	  <p><font size="4"><?php echo nl2br ($tekst); ?></p></font>
+	  <div class="grid_8 alpha">
+	<?php }?>
 </center>
 <div class="grid_13">
 
